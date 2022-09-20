064adc1706c39793687460783d374964bf9d4ba4
jbenoit12 <60207552+jbenoit12@users.noreply.github.com>
2022-09-20 17:08:18 -0400

    C21

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,25 +1,44 @@
 import React, { Component } from "react";
 import Zmage from "react-zmage";
 import Fade from "react-reveal";
+import ArchImg from "./07.jpg";
+import DataImg from "./04.jpg";
 
 let id = 0;
 class Portfolio extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const projects = this.props.data.projects.map(function (projects) {
-      let projectImage = "images/portfolio/" + projects.image;
 
-      return (
-        <div key={id++} className="columns portfolio-item">
-          <div className="item-wrap">
-            <Zmage alt={projects.title} src={projectImage}/>
-            <div style={{ textAlign: "center" }}>{projects.title}</div>
+    const projects = (<> <div key={id++} className="columns portfolio-item">
+                      <div className="item-wrap">
+                          <Zmage alt={"Data Sonification"} src={DataImg}/>
+                          <div style={{ textAlign: "center" }}>{<a href={"https://www.youtube.com/watch?v=Kb01eM7laDg"}>Data Sonification</a>}</div>
+                        
+                        </div>
+                      </div>
+                      
+                      <div key={id++} className="columns portfolio-item">
+                      <div className="item-wrap">
+                          <Zmage alt={"Field Archeology"} src={ArchImg}/>
+                          <div style={{ textAlign: "center" }}>{<a href={"https://www.facebook.com/archofcollegehill"}>Field Archeology</a>}</div>
+                        
+                        </div>
+                      </div>
+                      </>);
+    // const projects = this.props.data.projects.map(function (projects) {
+    //   let projectImage = "images/portfolio/" + projects.image;
+
+    //   return (
+    //     <div key={id++} className="columns portfolio-item">
+    //       <div className="item-wrap">
+    //         <Zmage alt={projects.title} src={projectImage}/>
+    //         <div style={{ textAlign: "center" }}>{<a href={"www.google.com"}>AAA</a>}</div>
             
-          </div>
-        </div>
-      );
-    });
+    //       </div>
+    //     </div>
+    //   );
+    // });
 
     return (
       <section id="portfolio">
@@ -33,7 +52,6 @@ class Portfolio extends Component {
                 className="bgrid-quarters s-bgrid-thirds cf"
               >
                 {projects}
-                
               </div>
             </div>
           </div>
